20ddf1193311b511b32f4a33930053e04af254e5
Abdessittir <harkati.web.dev@gmail.com>
2023-11-06 17:19:12 +0100

    Set prop types for AddButton

--- frontend/components/AddButton/index.tsx
+++ frontend/components/AddButton/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AddButton = ({ showPortal, text }) => {
+const AddButton = ({ showPortal, text }: { showPortal: () => void, text: string }) => {
     return (
         <button type="button" onClick={showPortal}>{text}</button>
     )
